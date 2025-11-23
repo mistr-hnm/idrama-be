1800d987134c0e9afff4af56f58746d2459f12a1
hnm <mistrhnm@gmail.com>
2025-11-23 20:12:37 +0530

    Update: remove unwanted dependancies

--- src/main.ts
+++ src/main.ts
@@ -5,28 +5,48 @@ import { ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 import { BadRequestException } from '@nestjs/common';
 
+import serverlessExpress from "@vendia/serverless-express";
+import { Callback, Context, Handler } from "aws-lambda";
+
+let server: Handler;
 async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  // // Cors config
+  // app.enableCors()
+  // app.use(graphqlUploadExpress())
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     exceptionFactory: (errors: ValidationError[]) => {
+  //       const errorMessage = errors.map(error =>
+  //         Object.values(error.constraints).join(', ')
+  //       )
+
+  //       const errorMessageParse = errorMessage
+  //         .map(error => error.charAt(0).toUpperCase() + error.slice(1))
+  //         .join(', ')
+
+  //       return new BadRequestException(String(errorMessageParse))
+  //     },
+  //     forbidUnknownValues: false,
+  //   })
+  // )
+  // await app.listen(process.env.PORT ?? 3000);
+
   const app = await NestFactory.create(AppModule);
-  // Cors config
-  app.enableCors()
-  app.use(graphqlUploadExpress())
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      exceptionFactory: (errors: ValidationError[]) => {
-        const errorMessage = errors.map(error =>
-          Object.values(error.constraints).join(', ')
-        )
+  await app.init();
 
-        const errorMessageParse = errorMessage
-          .map(error => error.charAt(0).toUpperCase() + error.slice(1))
-          .join(', ')
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 
-        return new BadRequestException(String(errorMessageParse))
-      },
-      forbidUnknownValues: false,
-    })
-  )
-  await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+// bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
